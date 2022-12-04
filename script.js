00a1bb1710b52858394b25fafa68436fc5edac7f
Nishita Pande <pande.nishita@gmail.com>
2022-12-04 08:56:21 +0530

    Modified

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const toggles = document.querySelectorAll(".faq-toggle");
+// const toggles = document.querySelectorAll(".faq-toggle");
 
-toggles.forEach((toggle) => {
-  toggle.addEventListener("click", () => {
-    toggle.parentNode.classList.toggle("active");
-  });
+// toggles.forEach((toggle) => {
+//   toggle.addEventListener("click", () => {
+//     toggle.parentNode.classList.toggle("active");
+//   });
+// });
+const yearEl=document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent= currentYear;
+
+
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click",function(){
+  headerEl.classList.toggle("nav-open");
 });
